aedf5aaa64d87e1070a1f5f4128d7c73f6737fce
橋本文瑠 <take@takeru.local>
2019-04-21 20:24:17 +0900

    Second commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ApplicationRecord
 	validates :user_id,{presence: true}
-	validates :post_image,{presence: true}
-	
-	mount_uploader :post_image, ImgUploader
 
 	def user
 		return User.find_by(id: self.user_id)
--- db/migrate/20190417173139_create_posts.rb
+++ db/migrate/20190417173139_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.integer :user_id
-      t.string :post_image
+      t.string :image_name
       t.string :comment
 
       t.timestamps
