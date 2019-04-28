a73a4774798e5a2cf83498032b808effa17975ad
橋本文瑠 <take@takeru.local>
2019-04-29 00:31:11 +0900

    オススメ画像登録成功

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,25 +1,57 @@
 $(function(){
 
 	setTimeout("$('#flash').fadeOut('slow')",1500);
-	
 
-	$('.prev-btn').click(function(){
-		var recpost_id_now = $('.recpost').data('id');
+	var post_array = [];
+	var post_num = [];
+	var recpost_id_now = $('.recpost').data('id');
+
+	//<li>作成
+	function createRecPost(post,id_now,i){
+		post_array.push(post);
+		post_num.push(i);
+
+		$('.recpost ul').append('<li value='+i+'><img></li>');
+		$('.recpost').find('li').attr({
+			class: 'slide'
+		});
+	}
+
+	//<li>にオススメ画像登録
+	function slidePost(){
+		for(var i=0;i<post_num.length;i++){
+
+			if ($('.recpost').find('li').val() == 0){
+				$('.recpost').find('li:eq(0)').addClass('active');
+			}
+			var image_name = post_array[i].image_name;
+			$('.recpost').find('img:eq('+i+')').attr({
+				alt: ' ',
+				src: '/post_images/'+image_name+''
+			});
+		}
+	}
+
+	//旬物(true)の分だけデータ取得
+	$(window).load(function(){
 		$.ajax({
 			url: '/',
 			type: 'GET',
 			data: {recpost: {id: recpost_id_now, rec: true}},
+			context: recpost_id_now,
 			dataType: 'json'
 		})
 		.done(function(data){
-			$('.recpost > img').attr('src','post_images/'+data[0].image_name+'');
-
-			var recpost_id_next = data[0].id;
-
+			$.each(data, function(i,data){
+				createRecPost(data,recpost_id_now,i);
+			});
+			slidePost();
 		})
 		.fail(function(){
-			alert('エラー');
+			alert('error');
 		})
 	});
+
+
 
 }); 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,9 @@ class HomeController < ApplicationController
   	@recpost = Post.find_by(rec: true)
   	respond_to do |format|
   		format.html
-  		format.json { @new_recpost = Post.where('id > ? and rec = ?', params[:recpost][:id], true) }
+  		format.json { @new_recpost = Post.where('id >= ? and rec = ?', params[:recpost][:id], true)}
   	end
-
   	@posts = Post.order(created_at: :desc).limit(9)
   end
 
-  
 end
