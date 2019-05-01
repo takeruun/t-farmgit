f73bf39430f8eee8c6a05acd131288fa3e5111e5
橋本文瑠 <take@takeru.local>
2019-05-01 12:07:58 +0900

    mysqlに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_155805) do
+ActiveRecord::Schema.define(version: 2019_05_01_024531) do
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.string "image_name"
     t.string "comment"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_155805) do
     t.boolean "rec"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
