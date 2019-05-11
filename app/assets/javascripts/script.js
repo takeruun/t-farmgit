d6df1438132858217a10f43a053a83a16d3e6242
橋本文瑠 <take@takeru.local>
2019-05-11 15:18:28 +0900

    新規投稿ページUI変更

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -68,5 +68,26 @@ $(function(){
 			$changePost.next().addClass('active');
 		}
 	});
-	
+
+	$('.form').on('change','input[type="file"]',function(e) {
+		var file = e.target.files[0];
+		var reader = new FileReader();
+
+		if(file.type.indexOf("image")<0){
+			return false;
+		}
+
+		reader.onload = (function(file){
+			return function(e){
+				$('.preview').empty();
+				$('.preview').append($('<img>').attr({
+					src: e.target.result,
+					alt: ' '
+				}));
+			};
+		})(file);
+
+		reader.readAsDataURL(file);
+	});
+
 }); 
