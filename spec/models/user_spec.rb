a68ecbd83851bb6a96581bd954dd798adf9f4907
橋本文瑠 <take@takeru.local>
2019-06-03 02:03:05 +0900

    userテスト法 変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "ユーザー作成できる" do
-  	user=User.new(
+  before do
+  	@user=User.create(
   		name: "パッポー",
   		image_name: "example.jpg",
   		email: "pa@example.com",
   		password: "akeebUen"
   		)
-  	expect(user).to be_valid
+  end
+
+  it "ユーザー作成できる" do
+  	expect(@user).to be_valid
   end
 
   it "名前ちゃんとある" do
-  	user=User.new(name:nil)
+  	user=User.new(name: nil)
   	user.valid?
-  	expect(user.errors[:name]).to_not include("can't be blank")
+  	expect(user.errors[:name]).to include("を入力してください")
+
   end
 
   it "写真の名前ちゃんとある" do
-  	user=User.new(image_name:nil)
+  	user=User.new(image_name: nil)
   	user.valid?
-  	expect(user.errors[:image_name]).to_not include("can't be blank")
+  	expect(user.errors[:image_name]).to include("を入力してください")
   end
 
   it "メアド重複しない" do
-  	User.create(
-  		name: "パッポー",
-  		image_name: "example.jpg",
-  		email: "pa@example.com",
-  		password: "akeebUen"
-  		)
   	user=User.new(
   		name: "ari",
   		image_name: "example.jpg",
@@ -37,7 +35,7 @@ RSpec.describe User, type: :model do
   		password: "akeebUen"
   		)
   	user.valid?
-  	expect(user.errors[:email]).to_not include("has alredy been taken")
+  	expect(user.errors[:email]).to include("はすでに存在します")
   end
 
 end
