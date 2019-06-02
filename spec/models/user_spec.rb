90c1ba7ca8df3467ef9fc0e7fcf7428fe4215143
橋本文瑠 <take@takeru.local>
2019-06-02 20:30:46 +0900

    userモデル　検証

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "ユーザー作成できる" do
+  	user=User.new(
+  		name: "パッポー",
+  		image_name: "example.jpg",
+  		email: "pa@example.com",
+  		password: "akeebUen"
+  		)
+  	expect(user).to be_valid
+  end
+
+  it "名前ちゃんとある" do
+  	user=User.new(name:nil)
+  	user.valid?
+  	expect(user.errors[:name]).to_not include("can't be blank")
+  end
+
+  it "写真の名前ちゃんとある" do
+  	user=User.new(image_name:nil)
+  	user.valid?
+  	expect(user.errors[:image_name]).to_not include("can't be blank")
+  end
+
+  it "メアド重複しない" do
+  	User.create(
+  		name: "パッポー",
+  		image_name: "example.jpg",
+  		email: "pa@example.com",
+  		password: "akeebUen"
+  		)
+  	user=User.new(
+  		name: "ari",
+  		image_name: "example.jpg",
+  		email: "pa@example.com",
+  		password: "akeebUen"
+  		)
+  	user.valid?
+  	expect(user.errors[:email]).to_not include("has alredy been taken")
+  end
+
 end
