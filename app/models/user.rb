d0a3846f088e1a4fbb0c7fd8e450851b0991fd7e
橋本文瑠 <take@takeru.local>
2019-06-02 17:50:10 +0900

    user validates changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 	validates :name,{presence: true}
 	validates :email,{presence: true, uniqueness: true}
 	validates :password,{presence: true}
+	validates :image_name,{presence: true}
 
 	has_many :posts, dependent: :destroy
 	has_many :favorites, dependent: :destroy
