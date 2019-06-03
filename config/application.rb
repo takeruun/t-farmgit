f86ae5ea9c298fdd8659e240973ad8da507936ed
橋本文瑠 <take@takeru.local>
2019-06-03 23:16:24 +0900

    postモデルテスト 追加

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module TFarm
          helper_specs: false,
          routing_specs: false
     end
+    
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  	@post=Post.create(
+  		user_id: 2,
+  		image_name: "1.jpg",
+  		comment: "exampleですよ",
+  		rec: true
+  		)
+  end
+
+  it "投稿できる" do
+  	expect(@post).to be_valid
+  end
+
+  it "ユーザーIDがない" do
+  	post=Post.new(user_id: nil)
+  	post.valid?
+  	expect(post.errors[:user_id]).to include("を入力してください")
+  end
+
+  it "画像無しだと投稿できない" do
+  	post=Post.new(image_name: nil)
+  	post.valid?
+  	expect(post.errors[:image_name]).to include("を入力してください")
+  end
+
+  it "コメント" do
+  	post=Post.new(comment: nil)
+  	post.valid?
+  	expect(post.errors[:comment]).to include("を入力してください")
+  end
 end
