4b15d5e148a297aea35b61d45ba7b93aacefa199
橋本文瑠 <take@takeru.local>
2019-06-04 22:57:03 +0900

    factoy 使用

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,41 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-  	@user=User.create(
-  		name: "パッポー",
-  		image_name: "example.jpg",
-  		email: "pa@example.com",
-  		password: "akeebUen"
-  		)
-  end
-
+  
   it "ユーザー作成できる" do
-  	expect(@user).to be_valid
+  	user = build(:user)
+  	expect(user).to be_valid
   end
 
   it "名前ちゃんとある" do
-  	user=User.new(name: nil)
+  	user = build(:user, name: nil)
   	user.valid?
   	expect(user.errors[:name]).to include("を入力してください")
 
   end
 
   it "写真の名前ちゃんとある" do
-  	user=User.new(image_name: nil)
+  	user = build(:user,image_name: nil)
   	user.valid?
   	expect(user.errors[:image_name]).to include("を入力してください")
   end
 
   it "メアド重複しない" do
-  	user=User.new(
-  		name: "ari",
-  		image_name: "example.jpg",
-  		email: "pa@example.com",
-  		password: "akeebUen"
-  		)
-  	user.valid?
-  	expect(user.errors[:email]).to include("はすでに存在します")
+  	user1 = build(:user1)
+  	user1.valid?
+  	expect(user1.errors[:email]).to include("はすでに存在します")
   end
 
 end
