e4fecb2b910044eb0f3d899f1cbbe046afc4cead
橋本文瑠 <take@takeru.local>
2019-06-04 22:56:00 +0900

    factory 実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+	factory :user do
+		name {"パッポー"}
+  		image_name {"example.jpg"}
+  		email {"pa@example.com"}
+  		password {"akeebUen"}
+	end
+
+	factory :user1, class: User do
+		name {"ほぐ"}
+  		image_name {"example.jpg"}
+  		email {"pa@example.com"}
+  		password {"akeebUen"}
+  	end
+end
