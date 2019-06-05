258994dc47ecfdfe9818b4d01858a7e7dbc96601
橋本文瑠 <take@takeru.local>
2019-06-05 22:45:53 +0900

    userテスト let使用 テスト項目(password)追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  let!(:user){create(:user)}
+  let(:user1){build(:user)}
+
   it "ユーザー作成できる" do
-  	user = build(:user)
   	expect(user).to be_valid
   end
 
-  it "名前ちゃんとある" do
-  	user = build(:user, name: nil)
+  it "名前がないと作成できない" do
+  	user.name = nil
   	user.valid?
   	expect(user.errors[:name]).to include("を入力してください")
-
   end
 
-  it "写真の名前ちゃんとある" do
-  	user = build(:user,image_name: nil)
+  it "写真を登録しないと作成できない" do
+  	user.image_name = nil
   	user.valid?
   	expect(user.errors[:image_name]).to include("を入力してください")
   end
 
-  it "メアド重複しない" do
-  	user1 = build(:user1)
+  it "パスワードがないと作成できない" do
+    user.password = nil
+    user.valid?
+    expect(user.errors[:password]).to include("を入力してください")
+  end
+
+  it "パスワードが6文字以上じゃないと作成できない" do
+    user.password = "Tt"
+    user.valid?
+    expect(user.errors[:password]).to include("は6文字以上で入力してください")
+  end
+
+  it "メアド重複すると作成できない" do
   	user1.valid?
   	expect(user1.errors[:email]).to include("はすでに存在します")
   end
