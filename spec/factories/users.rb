f4c57b2cd7504968dad96ef8905e72d2515c160b
橋本文瑠 <take@takeru.local>
2019-06-05 23:00:04 +0900

    user factory微調整

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
 	factory :user do
-		name {"パッポー"}
+		  name {"パッポー"}
   		image_name {"example.jpg"}
   		email {"pa@example.com"}
   		password {"akeebUen"}
 	end
 
 	factory :user1, class: User do
-		name {"ほぐ"}
+		  name {"ほぐ"}
   		image_name {"example.jpg"}
   		email {"pa@example.com"}
   		password {"akeebUen"}
