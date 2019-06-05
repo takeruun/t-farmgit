d345e852deeb8203d8e410fe9e4ceacbdb47e39d
橋本文瑠 <take@takeru.local>
2019-06-05 23:07:17 +0900

    let 使用

---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+	factory :post do
+		user_id {"2"}
+		image_name {"2.jpg"}
+		comment {"exampleです"}
+		rec {true}
+	end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,34 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  before do
-  	@post=Post.create(
-  		user_id: 2,
-  		image_name: "1.jpg",
-  		comment: "exampleですよ",
-  		rec: true
-  		)
-  end
+  let!(:post){create(:post)}
 
   it "投稿できる" do
-  	expect(@post).to be_valid
+  	expect(post).to be_valid
   end
 
-  it "ユーザーIDがない" do
-  	post=Post.new(user_id: nil)
+  it "ユーザーIDがないと投稿できない" do
+  	post.user_id = nil
   	post.valid?
   	expect(post.errors[:user_id]).to include("を入力してください")
   end
 
   it "画像無しだと投稿できない" do
-  	post=Post.new(image_name: nil)
+  	post.image_name = nil
   	post.valid?
   	expect(post.errors[:image_name]).to include("を入力してください")
   end
 
-  it "コメント" do
-  	post=Post.new(comment: nil)
+  it "コメントがないと投稿できない" do
+  	post.comment = nil
   	post.valid?
   	expect(post.errors[:comment]).to include("を入力してください")
   end
+  
 end
