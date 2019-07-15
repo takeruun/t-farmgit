4585ae6849338243dfa2b7294723313deccfe415
橋本文瑠 <take@takeru.local>
2019-07-16 00:40:37 +0900

    前回のコミット忘れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  	devise :database_authenticatable, :registerable,
-    	     :recoverable, :rememberable, :validatable
-	validates :name,{presence: true}
-	validates :image_name,{presence: true}
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-	has_many :posts, dependent: :destroy
-	has_many :favorites, dependent: :destroy
-	has_many :fav_posts, through: :favorite, source: :post
-
+  validates :name, {presence: true}
+  validates :image_name, {presence: true}
+  
 end
