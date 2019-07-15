7f513a309976c2f2d8a4883db3af4596a1b1eec3
橋本文瑠 <take@takeru.local>
2019-07-15 14:26:04 +0900

    メアド・パスワード変更テスト追記

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,15 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Uesr", type: :systen do
-	let(:user){build(:user)}
+RSpec.describe Uesr, type: :systen do
+	let(:user){create(:user)}
 
-	it "ユーザーログインできるか" do
+	before do
 		visit new_user_session_path
 		fill_in "メールアドレス", with: user.email
 		fill_in "パスワード", with: user.password
 		click_button 'ログイン'
-		expect(page).to have_content 'ようこそ'
 	end
 
+	describe "編集機能" do
+		it 'メールアドレス変更できる' do
+			visit edit_user_registration_path(user)
+			fill_in "メールアドレス", with: 'test@example.com'
+			fill_in "現在のパスワード", with: user.password
+			click_button '更新'
+			expect(user.reload.email).to eq 'test@example.com'
+		end
+
+		it 'パスワード変更できる' do
+			visit edit_user_registration_path(user)
+			fill_in "メールアドレス", with: user.email
+			fill_in "パスワード", with: 'example'
+			fill_in "確認用パスワード", with: 'example'
+ 			fill_in "現在のパスワード", with: user.password
+			click_button '更新'
+			user.reload
+			expect(user.valid_password?("example")).to eq(true)
+		end
+	end
 
 end
