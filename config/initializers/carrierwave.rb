5d5f2b90b707d4176747b82979545653adf3a68b
橋本文瑠 <take@takeru.local>
2019-07-23 02:33:46 +0900

    aws s3 アップロード設定追記

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,23 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
 
-  storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.aws[:s3_access_key_id],
     aws_secret_access_key: Rails.application.credentials.aws[:s3_secret_access_key],
-    region: Rails.application.credentials.aws[:aws_region],
+    region: Rails.application.credentials.aws[:region],
     path_style: true
   }
 
-  config.fog_directory = 't-farm-backet' if Rails.env.production?
-  config.fog_directory = 't-farm-for-dev' if Rails.env.development?
+  if Rails.env.production?
+    config.fog_directory = 't-farm-bucket'
+  elsif Rails.env.development?
+    config.fog_directory = 't-farm-bucket-for-dev'
+  end
 
 end
  
