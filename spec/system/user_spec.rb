fc0543aa0f1722ad25a64ea49a67f6420825e468
橋本文瑠 <take@takeru.local>
2019-07-25 01:54:04 +0900

    attach_file pending

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,22 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Uesr, type: :systen do
+RSpec.describe User, type: :system do
 	let(:user){create(:user)}
-
-	before do
-		visit new_user_session_path
-		fill_in "メールアドレス", with: user.email
-		fill_in "パスワード", with: user.password
-		click_button 'ログイン'
-	end
-
+	
 	describe "編集機能" do
+		before do
+			visit new_user_session_path
+			fill_in "メールアドレス", with: user.email
+			fill_in "パスワード", with: user.password
+			click_button 'ログイン'
+		end
+
 		it 'メールアドレス変更できる' do
 			visit edit_user_registration_path(user)
 			fill_in "メールアドレス", with: 'test@example.com'
 			fill_in "現在のパスワード", with: user.password
 			click_button '更新'
 			expect(user.reload.email).to eq 'test@example.com'
+		end
+
+		it '画像変更できる' do
+			visit edit_user_registration_path(user)
+			fill_in "メールアドレス", with: user.email
+			pending "fieldがないと出る"
+			attach_file image, with: File.join(Rails.root, 'public/user_images/a.jpg')
+ 			fill_in "現在のパスワード", with: user.password
+			click_button '更新'
+			expect(user.reload.image).to eq "example.jpg"
 		end
 
 		it 'パスワード変更できる' do
@@ -29,6 +39,8 @@ RSpec.describe Uesr, type: :systen do
 			user.reload
 			expect(user.valid_password?("example")).to eq(true)
 		end
+
+		
 	end
 
 end
