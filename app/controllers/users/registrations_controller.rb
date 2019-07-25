bdfe7684796ddefabb74fbc3cdb1afa7b32e616d
橋本文瑠 <take@takeru.local>
2019-07-25 14:30:30 +0900

    画像編集追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,11 +17,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	end
 
 	def update
-		super
+	   @user = User.find_by(id: current_user.id)
+	   super
+	   if params[:user][:image]
+	   	@user.image = params[:user][:image]
+	   	@user.save
+	   	flash[:notice] = "画像編集しました"
+	   end
 	end
 
 	def destroy
 		super
 	end
 
+	protected
+     def user_params
+       params.require(:user).permit(:image, :email, :password ,:password_confirmation, :name, keys: [:current_password] )
+     end
 end
