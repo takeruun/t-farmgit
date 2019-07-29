11caf2a3291100cf42331f838dc3a80f2200765f
橋本文瑠 <take@takeru.local>
2019-07-29 15:05:00 +0900

    post image追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
 	factory :post do
-		user_id {"2"}
-		image {"2.jpg"}
+		user_id {2}
+		image {Rack::Test::UploadedFile.new(File.join(Rails.root, 'public/post_images/1.jpg'))}
 		comment {"exampleです"}
-		rec {true}
-		title {"タイトル"}
+		fav_count {4}
+		title {"example"}
 	end
 end
