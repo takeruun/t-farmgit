656fb3603e1de936fa6e0b69392fa97dfa7ca56c
橋本文瑠 <take@takeru.local>
2019-09-07 02:28:57 +0900

    fav_count項目削除

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,14 +2,12 @@ FactoryBot.define do
 	factory :post do
 		image {Rack::Test::UploadedFile.new(File.join(Rails.root, 'public/post_images/1.jpg'))}
 		comment {"exampleです"}
-		fav_count {4}
 		title {"example"}
 	end
 
 	factory :other_post, class: Post do
 		image {Rack::Test::UploadedFile.new(File.join(Rails.root, 'public/post_images/1.jpg'))}
 		comment {"other_exampleです"}
-		fav_count {3}
 		title {"other_example"}
 	end
 end
