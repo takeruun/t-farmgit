951e72f01ec44ab29d89d3b8eb375ffbd6b86a2b
橋本文瑠 <take@takeru.local>
2020-01-10 15:01:43 +0900

    chrome test追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -20,7 +22,31 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+Capybara.configure do |config|
+  config.server_host = "web.com"
+  #config.server_port = 3000
+  config.javascript_driver = :selenium_chrome_headless
+end
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: {
+        args: [
+          "window-size=1024,512",
+          "--no-sandbox",
+          "--disable-dev-shm-usage"
+        ]
+      }
+    ),
+    url: "http://chrome.com:4444/wd/hub",
+  )
+end
+
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
