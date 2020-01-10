de8c92abd705ad7297797719b8ac11dd9821af3d
橋本文瑠 <take@takeru.local>
2020-01-10 15:20:29 +0900

    削除やら認証やら

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, {only: [:new, :create, :edit, :update, :destroy]}
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
 
   def index
     @favpost = Post.order(fav_count: :desc).where('fav_count > ?', 0).limit(4).last
@@ -41,15 +43,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     if @post.update(post_params)
       flash[:notice] = "編集しました"
-      redirect_to("posts/#{@post.id}")
+      redirect_to("/posts/#{@post.id}")
     else
-      render('post/edit')
+      flash[:notice] = @post.errors.full_messages
+      render("/posts/edit")
     end
 
+  end
+
+  def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      flash[:notice] = "投稿を削除しました"
+      redirect_to('/posts')
   end
 
   private
   def post_params
     params.require(:post).permit(:comment,:title,:image,:amount,:from)
   end
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    if current_user.id != @post.user_id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts")
+    end
+  end
+
 end
