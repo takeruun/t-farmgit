9fc8a970b7ada0507a836982b94323c59d520036
橋本文瑠 <take@takeru.local>
2020-02-23 13:40:44 +0900

    test環境のとき場合わけ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,21 +3,25 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:s3_access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:s3_secret_access_key],
-    region: Rails.application.credentials.aws[:region],
-    path_style: true
-  }
-  config.fog_public = false
+  if Rails.env.production? || Rails.env.development?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:s3_access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:s3_secret_access_key],
+      region: Rails.application.credentials.aws[:region],
+      path_style: true
+    }
+    config.fog_public = false
 
-  if Rails.env.production?
-    config.fog_directory = 't-farm-bucket'
-  elsif Rails.env.development?
-    config.fog_directory = 't-farm-bucket-for-dev'
+    if Rails.env.production?
+      config.fog_directory = 't-farm-bucket'
+    elsif Rails.env.development?
+      config.fog_directory = 't-farm-bucket-for-dev'
+    end
+
+  else
+    config.storage :file
   end
 end
-
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
